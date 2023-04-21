4829b723a0216d107fd3671ae8edc54a4493eb8b
Art <amolc@pm.me>
2023-04-21 20:52:02 +0100

    test: Add tests for generic consts

--- delegate-display-testing/src/lib.rs
+++ delegate-display-testing/src/lib.rs
@@ -2,46 +2,62 @@
 
 #![allow(dead_code)]
 
-use delegate_display::*;
+pub mod enums {
+    use delegate_display::*;
 
-#[derive(DelegateDisplay, DelegateDebug)]
-struct SA;
+    #[derive(DelegateDisplay, DelegateDebug)]
+    pub enum EA {}
 
-#[derive(DelegateDisplay, DelegateDebug)]
-struct SB {}
+    #[derive(DelegateDisplay, DelegateDebug)]
+    pub enum EB {
+        A,
+        B(EA),
+        C { x: super::structs::SE },
+    }
 
-#[derive(DelegateDisplay, DelegateDebug)]
-struct SC {}
-
-#[derive(DelegateDisplay, DelegateDebug)]
-struct SD(SA);
-
-#[derive(DelegateDisplay, DelegateDebug)]
-struct SE {
-    x: SB,
+    #[derive(DelegateDebug)]
+    pub enum EC<T: core::fmt::Debug> {
+        A,
+        B(T),
+        C { val: String },
+    }
 }
 
-#[derive(DelegateDisplay)]
-struct SF<T>(T)
-where
-    T: core::fmt::Display;
+pub mod structs {
+    use delegate_display::*;
 
-#[derive(DelegateDisplay)]
-struct SG<T: core::fmt::Display = u8>(T);
+    #[derive(DelegateDisplay, DelegateDebug)]
+    pub struct SA;
 
-#[derive(DelegateDisplay, DelegateDebug)]
-enum EA {}
+    #[derive(DelegateDisplay, DelegateDebug)]
+    pub struct SB {}
 
-#[derive(DelegateDisplay, DelegateDebug)]
-enum EB {
-    A,
-    B(EA),
-    C { x: SE },
-}
+    #[derive(DelegateDisplay, DelegateDebug)]
+    pub struct SC {}
 
-#[derive(DelegateDebug)]
-enum EC<T: core::fmt::Debug> {
-    A,
-    B(T),
-    C { val: String },
+    #[derive(DelegateDisplay, DelegateDebug)]
+    pub struct SD(SA);
+
+    #[derive(DelegateDisplay, DelegateDebug)]
+    pub struct SE {
+        x: SB,
+    }
+
+    #[derive(DelegateDisplay)]
+    pub struct SF<T>(T)
+    where
+        T: core::fmt::Display;
+
+    #[derive(DelegateDisplay)]
+    pub struct SG<T: core::fmt::Display = u8>(T);
+
+    pub struct SHInner<T: core::fmt::Display, const C: usize>([T; C]);
+    impl<T: core::fmt::Display, const C: usize> core::fmt::Display for SHInner<T, C> {
+        fn fmt(&self, _: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+            Ok(())
+        }
+    }
+
+    #[derive(DelegateDisplay)]
+    pub struct SH<T: core::fmt::Display, const C: usize = 5>(SHInner<T, C>);
 }
